ce3f36fc485038bf5d7996a4f062fb6e985c421a
tadakado (Tadashi Kadowaki) <tadakado@gmail.com>
2008-06-09 18:12:46 +0000

    small change
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pdf2@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/src/devPS.c
+++ trunk/src/devPS.c
@@ -3,6 +3,7 @@
  *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
  *  Copyright (C) 1998--2008  Robert Gentleman, Ross Ihaka and the
  *                            R Development Core Team
+ *  Copyright (C) 2008        Tadashi Kadowaki
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
