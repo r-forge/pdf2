f25da47dceba6d1b74e1f77aa736aaca8a8afe29
tobias_verbeke (Tobias Verbeke) <tobias.verbeke@telenet.be>
2008-06-11 17:02:15 +0000

    minor changes + added help page templates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pdf2@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/demo/pdf2.r
+++ trunk/demo/pdf2.r
@@ -13,7 +13,7 @@ pdf.box(1, 5, 3, 8, "http://www.r-project.org")
 ## You can also make popup text box by "pdf.box" ##
 ###################################################
 for(i in 1:10)
-  pdf.box(i-0.1, i-0.1, i+0.1, i+0.1, LETTERS[i], link=F, border=c(0,0,0))
+  pdf.box(i-0.1, i-0.1, i+0.1, i+0.1, LETTERS[i], link=FALSE, border=c(0,0,0))
 text(5, 9, "mouse over points!", col=2)
 
 ############################################################
@@ -24,7 +24,7 @@ text(5, 2, "text test", url="http://www.google.com")
 
 mtext("mtext test 1", cex=2, popup="Hello, world!")
 mtext("mtext test 2", side = 2, line=2, url="http://www.apple.com")
-mtext("mtext test 3", outer=T, side = 1, url="http://www.bioconductor.org")
-mtext("mtext test 4", outer=T, side = 4, cex=3, url="http://www.playstation.com")
+mtext("mtext test 3", outer=TRUE, side = 1, url="http://www.bioconductor.org")
+mtext("mtext test 4", outer=TRUE, side = 4, cex=3, url="http://www.playstation.com")
 
 dev.off()
