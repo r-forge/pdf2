ce7793c14d0f4742fd1baf72a79cbebcaf0c5c1f
tadakado (Tadashi Kadowaki) <tadakado@gmail.com>
2009-11-02 16:24:49 +0000

    20091103
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pdf2@120 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/demo/pdf2.r
@@ -0,0 +1,33 @@
+# This is a sample for extending pdf device
+library(pdf2)
+
+pdf("pdf2.pdf")
+par(oma=c(2,2,2,2))
+
+plot(1:10)
+
+###########################################################
+## You can make URL link box by "rect" with "url" option ##
+###########################################################
+rect(1, 5, 3, 8, border="cyan", url="http://www.r-project.org")
+
+####################################################################
+## You can also make popup text box by "rect" with "popup" option ##
+####################################################################
+x <- 1:10
+rect(x-0.1, x-0.1, x+0.1, x+0.1, border=NA, popup=LETTERS[x])
+
+text(5, 9, "mouse over points!", col=2)
+
+############################################################
+## URL link and popup text for strings can be assigned by ##
+## "text" and "mtext".                                    ##
+############################################################
+text(5, 2, "text test", url="http://www.google.com")
+
+mtext("mtext test 1", cex=2, popup="Hello, world!")
+mtext("mtext test 2", side = 2, line=2, url="http://www.apple.com")
+mtext("mtext test 3", outer=TRUE, side = 1, url="http://www.bioconductor.org")
+mtext("mtext test 4", outer=TRUE, side = 4, cex=3, url="http://www.playstation.com", pcol=NA)
+
+dev.off()
